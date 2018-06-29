bcf8db5449514609f751126f19adc45a1b6df0bd
cxlegge <38881277+cxlegge@users.noreply.github.com>
2018-06-29 14:27:47 +0100

    Update Scriptmaker.py
    Comments make more sense.

--- Scriptmaker.py
+++ Scriptmaker.py
@@ -5,8 +5,9 @@ lines = start.readlines()
 end = open('videoscript.txt', 'w+')
 lines2 = end.readlines()
 for line in lines:
-  ''' Code below removes lines with colons and line less than 4 characters long
-      then removes newlines to create the end document. '''
+  ''' Code below removes lines with colons without spaces after,
+      and lines less than 4 characters long.
+      Then removes newlines to create the end document. '''
   if not re.search(':\S', line) and len(line) > 4:
     end.write(line.replace('\n', ' '))
     
