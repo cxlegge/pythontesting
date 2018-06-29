aec5caf419ef0a458903bc8c84e40e6ca48a5e07
cxlegge <38881277+cxlegge@users.noreply.github.com>
2018-06-29 10:42:03 +0100

    Update Scriptmaker.py
    Only issue is I need to create better parameters for removing timestamp lines.

--- Scriptmaker.py
+++ Scriptmaker.py
@@ -1,9 +1,10 @@
+# A way to create an easy-to-read script from downloaded YouTube subtitles.
 thing = open('caption_input.srt', 'r+')
 lines = thing.readlines()
 thing2 = open('videoscript.txt', 'w+')
 lines2 = thing2.readlines()
 for line in lines:
-  
+  # Code below removes lines with colons and line less than 4 characters long, then removes newlines to create the end document.
   if not ':' in line and len(line) > 4:
     thing2.write(line.replace('\n', ' '))
     
