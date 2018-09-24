c620724ab042a9b50f7d9c0a7d0606694528186b
cxlegge <38881277+cxlegge@users.noreply.github.com>
2018-09-24 09:46:20 +0100

    Update csvreader.py
    - Changed function to ask for user input for column reading instead of altering code.
    - Now counts empty cells and returns it

--- csvreader.py
+++ csvreader.py
@@ -1,19 +1,26 @@
 import pandas as pd
+# reads csv file column data, prompts user to input which column
+print('Please enter the target column of your csv file you wish to count.')
+title = input('Column title: ')
 
-pand = pd.read_csv('domains.csv', index_col=0)
-pand1 = pand.iloc[:,5]
-# where 5 is the target column
+try:
+  pand = pd.read_csv('domains.csv', index_col=0)
+  pand1 = pand.loc[:,title]
+except:
+    print('Something went wrong, either your column name is incorrect or out of range, did you check the spelling?')
 newlist = []
-
+nullacc = 0
 for i in pand1:
     if type(i) == str:
         newlist.append(i)
-# ^ removes empty cells
-
+        # only adds non-empty cells
+    else:
+        nullcount +=1
+        # counts empty cells
 newlist =  sorted(newlist)
 
 print(newlist)
-
+# optional print statement for the column's data
 groups = []
 
 for i in newlist:
@@ -21,3 +28,5 @@ for i in newlist:
         groups.append(i)
 for v in groups:
     print(v, newlist.count(v))
+
+print('There are %s missing %s values.' % (nullcount, title))
