de727903e174b755b885bf8f8c691adf3e908f58
cxlegge <38881277+cxlegge@users.noreply.github.com>
2018-10-11 14:03:59 +0100

    Update csvdatalister.py
    Improved readability by adding spaces between important sections

--- csvdatalister.py
+++ csvdatalister.py
@@ -4,16 +4,20 @@ aread = pd.read_csv('data.csv', index_col=0)
 #input target columns in the list below
 targetcols = ['column1','column2']
 f = open('dataset.txt', 'w+')
+
 #begins column looping, writes title of column to file, bounded by 20 dashes
 for targetcol in targetcols:
     list1 = list((aread.loc[:,targetcol]))
     f.write(('-'*20) + '\n' + targetcol + ' total: ' + str(len(list1)) + '\n' + ('-'*20) + '\n')
+    
     #creates holding list('group') for transfer to file
     groups = []
+    
     #adds unique, non-NaN values to the 'groups' list
     for i in list1:
         if i not in groups and str(i) != 'nan':
             groups.append(i)
+            
     #writes the number of items in the column, indexed by item in holding list('group'), then writes group item name
     for v in groups:
         f.write(str(list1.count(v)))
